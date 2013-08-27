0376cc70bbba48bdd876eccf4847ed8cbb451b6b
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-27 10:34:24 -0700

    implement skeleton for 7 RESTful routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,34 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  # display a list of all notes
+  @notes = Note.all
+  erb :index  
+end
+
+get '/new' do
+  # return an html form for creating a new note
+  erb :new
+end
+
+post '/' do
+  Note.create(params)
+  redirect '/'
+end
+
+get '/:id' do
+  # display a specific note
+  @note = Note.find(params[:id])
+  erb :note
+end
+
+get '/:id/edit' do
+  # return an html form for editing a note
+end
+
+put '/:id' do
+  # update a specific note
+end
+
+delete '/:id' do
+  # delete a specific note
+  Note.find(params[:id]).destroy
 end
