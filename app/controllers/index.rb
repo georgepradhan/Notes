b1b6d6b4c234ea5757af2a61ff368b286fc76f3e
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-27 11:00:37 -0700

    create view as a table. include link for delete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,6 +10,7 @@ get '/new' do
 end
 
 post '/' do
+  # create a new note
   Note.create(params)
   redirect '/'
 end
@@ -22,13 +23,28 @@ end
 
 get '/:id/edit' do
   # return an html form for editing a note
+  @note = Note.find(params[:id])
+  erb :edit
 end
 
 put '/:id' do
   # update a specific note
+  note = Note.find(params[:id])
+  note.title = params[:title]
+  note.content = params[:content]
+  note.save
+  redirect '/'
 end
+
+get '/:id/delete' do
+  # route to a confirmation page
+  @note = Note.find(params[:id])
+  erb :delete
+end
+
 
 delete '/:id' do
   # delete a specific note
   Note.find(params[:id]).destroy
+  redirect '/'
 end
